902583d4f65a597ddd8e60742c17bad6b32384ae
Emre Sert <emresert1233@gmail.com>
2019-09-08 19:47:59 +0300

    Progress bar examples with timer and text form object

---
+++ ToolTipUsage/ToolTipUsage/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToolTipUsage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Determine ToolTip Feature
+            toolTip1.ToolTipIcon = ToolTipIcon.Warning;
+            toolTip1.ToolTipTitle = "Uyarı!";
+            toolTip1.AutomaticDelay = 200;
+            toolTip1.AutoPopDelay = 3000;
+
+            toolTip2.ToolTipIcon = ToolTipIcon.Info;
+            toolTip2.ToolTipTitle = "Bilgilendirme.";
+            toolTip2.AutomaticDelay = 200;
+            toolTip2.AutoPopDelay = 2000;
+
+            // Tricks text
+            toolTip1.SetToolTip(this.textBox1, "Kişisel Bilgiler");
+            toolTip1.SetToolTip(this.textBox2, "Kullanıcı Adı 8 karakter olmalı");
+            toolTip1.SetToolTip(this.textBox3, "Şifre harf, sayı ve özel karakter içermelidir.");
+            toolTip1.SetToolTip(this.textBox4, "Lütfen şifrenizi doğrulayın.");
+            toolTip2.SetToolTip(this.textBox5, "Cep telefonu bilgisi zorunlu değil.");
+        }
+    }
+}
